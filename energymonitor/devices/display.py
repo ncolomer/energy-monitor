f534c70ba000612a1645035ee2535a23d7a6a95f
Nicolas Colomer <ncolomer@users.noreply.github.com>
2020-11-12 17:58:46 +0100

    Use adafruit's Adafruit_CircuitPython_SSD1305 library for display https://github.com/adafruit/Adafruit_CircuitPython_SSD1305

--- energymonitor/devices/display.py
+++ energymonitor/devices/display.py
@@ -1,9 +1,11 @@
 import logging
 
+import adafruit_ssd1305
+import board
+import digitalio
 from PIL import Image
+from adafruit_ssd1305 import SET_DISP
 
-import energymonitor.devices.ssd1305.SPI as SPI
-import energymonitor.devices.ssd1305.SSD1305 as SSD1305
 from energymonitor.helpers.constants import LOGO
 
 
@@ -15,27 +17,33 @@ class Display:
 
     def __init__(self):
         self.logger = logging.getLogger(self.__class__.__name__)
-        spi = SPI.SpiDev(port=0, device=0, max_speed_hz=8000000)
-        self._display = SSD1305.SSD1305_128_32(rst=None, dc=24, spi=spi)
+
+        self._oled = adafruit_ssd1305.SSD1305_SPI(
+            width=128, height=32,
+            spi=board.SPI(),
+            dc=digitalio.DigitalInOut(board.D5),
+            reset=digitalio.DigitalInOut(board.D4),
+            cs=digitalio.DigitalInOut(board.D6)
+        )
+
         # Initialize sequence
-        self._display.begin()
-        self._display.set_contrast(0)
+        self._oled.contrast(0xFF)
         self.print(LOGO)
         self.logger.info('Initialized')
 
     def image(self):
-        return Image.new(mode='1', size=(self._display.width, self._display.height), color=0)
-
-    def print(self, image):
-        self._display.image(image)
-        self._display.display()
+        return Image.new(mode='1', size=(self._oled.width, self._oled.height), color=0)
 
     def display_on(self):
-        self._display.command(SSD1305.SSD1305_DISPLAYON)
+        self._oled.write_cmd(SET_DISP | 0x01)
 
-    def display_off(self):
-        self._display.command(SSD1305.SSD1305_DISPLAYOFF)
+    def print(self, image):
+        self._oled.image(image)
+        self._oled.show()
 
     def display_clear(self):
-        self._display.clear()
-        self._display.display()
+        self._oled.fill(0)
+        self._oled.show()
+
+    def display_off(self):
+        self._oled.write_cmd(SET_DISP | 0x00)
