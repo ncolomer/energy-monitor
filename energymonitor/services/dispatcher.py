b12bad2af99f25b2c01b548f921843017bfa08ec
Nicolas Colomer <ncolomer@users.noreply.github.com>
2020-11-12 09:44:05 +0100

    Add subscription lock mechanism + proper thread name + log

--- energymonitor/services/dispatcher.py
+++ energymonitor/services/dispatcher.py
@@ -1,7 +1,7 @@
 import logging
 from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
-from threading import Thread
+from threading import Thread, Lock
 from typing import Callable
 
 
@@ -14,11 +14,15 @@ class Dispatcher(Thread):
         super().__init__(name=self.__class__.__name__)
         self.logger = logging.getLogger(self.__class__.__name__)
         self.queue = Queue()
+        self.subscription_lock = Lock()
         self.subscribers = []
 
     def subscribe(self, handler: Callable):
-        executor = ThreadPoolExecutor(thread_name_prefix='dispatcher', max_workers=1)
-        self.subscribers.append((executor, handler))
+        with self.subscription_lock:
+            subscriber_id = f'Subscriber{len(self.subscribers) + 1}'
+            executor = ThreadPoolExecutor(thread_name_prefix=f'{self.name}-{subscriber_id}', max_workers=1)
+            self.subscribers.append((executor, handler))
+            self.logger.debug('Added subscriber %s', subscriber_id)
 
     def publish(self, message):
         self.queue.put(message)
